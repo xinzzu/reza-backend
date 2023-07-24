df1f6d72567a63c17b4813fdf25d95451a796077
xinzzu <azisnurrahman54@gmail.com>
2023-07-24 17:46:21 +0700

    up

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,6 @@ router.post("/", async (req, res) => {
 				.status(409)
 				.send({ message: "User with given email already Exist!" });
 
-		const salt = await bcrypt.genSalt(Number(process.env.SALT));
-		const hashPassword = await bcrypt.hash(req.body.password, salt);
 
 		await new User({ ...req.body, password: hashPassword }).save();
 		res.status(201).send({ message: "User created successfully" });
